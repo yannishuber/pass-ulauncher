26949838e09ef835f497ee594363d2040cd7dbdb
Yannis Huber <yannis.h@hotmail.ch>
2019-01-07 17:52:24 +0100

    Add travis-ci

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+""" Yannis Huber 2019 """
+
+from subprocess import check_output
+from os import path as os_path
+import re
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
@@ -6,9 +11,6 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
 from ulauncher.api.shared.action.SetUserQueryAction import SetUserQueryAction
 from ulauncher.api.shared.action.DoNothingAction import DoNothingAction
-from subprocess import check_output
-from os import path as os_path
-import re
 
 PASSWORD_ICON = 'images/icon.png'
 FOLDER_ICON = 'images/folder.png'
@@ -25,7 +27,7 @@ WRONG_PATH_ITEM = ExtensionResultItem(icon=WARNING_ICON,
 MORE_ELEMENTS_ITEM = ExtensionResultItem(icon=MORE_ICON,
                                          name='Keep typing...',
                                          description='More items are available.'
-                                                     + ' Narrow your search by entering a pattern.',
+                                         + ' Narrow your search by entering a pattern.',
                                          on_enter=DoNothingAction())
 
 NO_FILENAME_ITEM = ExtensionResultItem(icon=WARNING_ICON,
@@ -35,12 +37,14 @@ NO_FILENAME_ITEM = ExtensionResultItem(icon=WARNING_ICON,
 
 
 class PassExtension(Extension):
+    """ Extension class, does the searching """
 
     def __init__(self):
         super(PassExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
     def search(self, path=None, pattern=None, depth=None):
+        """ Launches a find command with the specified pattern """
 
         store_location = os_path.expanduser(self.preferences['store-location'])
 
@@ -57,16 +61,20 @@ class PassExtension(Extension):
 
         searching_path = os_path.join(store_location, path)
 
-        cmd_files = "find {} {}-type f -not -path *.git* -not -name .* -iname *{}*".format(searching_path,
-                                                                                           max_depth,
-                                                                                           pattern)
+        cmd_files = "find {} {}-type f -not \
+                    -path *.git* -not -name .* -iname *{}*".format(searching_path,
+                                                                   max_depth,
+                                                                   pattern)
 
-        cmd_dirs = "find {} {}-type d -not -path *.git* -not -name .* -iname *{}*".format(searching_path,
-                                                                                          max_depth,
-                                                                                          pattern)
+        cmd_dirs = "find {} {}-type d -not \
+                    -path *.git* -not -name .* -iname *{}*".format(searching_path,
+                                                                   max_depth,
+                                                                   pattern)
 
-        files = re.findall("{}/*(.+).gpg".format(searching_path), check_output(cmd_files.split(" ")))
-        dirs = re.findall("{}/*(.+)".format(searching_path), check_output(cmd_dirs.split(" ")))
+        files = re.findall("{}/*(.+).gpg".format(searching_path),
+                           check_output(cmd_files.split(" ")))
+        dirs = re.findall("{}/*(.+)".format(searching_path),
+                          check_output(cmd_dirs.split(" ")))
 
         files.sort()
         dirs.sort()
@@ -75,37 +83,39 @@ class PassExtension(Extension):
 
 
 class KeywordQueryEventListener(EventListener):
+    """ KeywordQueryEventListener class used to manage user input"""
 
     def __init__(self):
         self.extension = None
 
     def render_results(self, path, files, dirs, keyword):
+        """ Prepares the results for the UI """
         items = []
         limit = int(self.extension.preferences['max-results'])
 
         if limit < len(dirs) + len(files):
             items.append(MORE_ELEMENTS_ITEM)
 
-        for d in dirs:
+        for _dir in dirs:
             limit -= 1
             if limit < 0:
                 break
 
             action = SetUserQueryAction("{0} {1}/".format(keyword,
-                                                          os_path.join(path, d)))
+                                                          os_path.join(path, _dir)))
             items.append(ExtensionResultItem(icon=FOLDER_ICON,
-                                             name="{0}".format(d),
+                                             name="{0}".format(_dir),
                                              description=FOLDER_DESCRIPTION,
                                              on_enter=action))
 
-        for f in files:
+        for _file in files:
             limit -= 1
             if limit < 0:
                 break
 
-            action = RunScriptAction("pass -c {0}/{1}".format(path, f), None)
+            action = RunScriptAction("pass -c {0}/{1}".format(path, _file), None)
             items.append(ExtensionResultItem(icon=PASSWORD_ICON,
-                                             name="{0}".format(f),
+                                             name="{0}".format(_file),
                                              description=PASSWORD_DESCRIPTION,
                                              on_enter=action))
 
@@ -169,11 +179,15 @@ class KeywordQueryEventListener(EventListener):
 
                 # If the user specified a pattern and we are in generation mode
                 # give him the possibility to generate the password
-                action = RunScriptAction("pass generate -c {}".format(os_path.join(path, pattern)), None)
-                misc.append(ExtensionResultItem(icon=PASSWORD_ICON,
-                                                name='Generate /{}'.format(os_path.join(path, pattern)),
-                                                description='Enter to generate and save password',
-                                                on_enter=action))
+                action = RunScriptAction(
+                    "pass generate -c {}".format(os_path.join(path, pattern)),
+                    None)
+
+                misc.append(ExtensionResultItem(
+                    icon=PASSWORD_ICON,
+                    name='Generate /{}'.format(os_path.join(path, pattern)),
+                    description='Enter to generate and save password',
+                    on_enter=action))
 
             # If the specified path doesn't exist it makes no sense to search for a password
             if not path_not_exists:
